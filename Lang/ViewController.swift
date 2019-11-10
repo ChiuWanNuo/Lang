ed5034f7b69bcf12ca7b8df6ff98a8699ba5b57f
Wan-Nuo, Chiu <sunnusbi@gmail.com>
2019-11-10 21:27:25 +0800

    20191110

---
+++ Lang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lang
+//
+//  Created by ChiuWanNuo on 2019/11/3.
+//  Copyright © 2019 ChiuWanNuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
